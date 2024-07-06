fac912722215e840a41634757174ce739c5bef31
raddevus <1radeutsch1@gmail.com>
2024-07-06 15:01:00 -0400

    Had to add asp-append-version to the shoplist.js script tag so newest version will always be loaded. Completed addList JS method so the list is posted to the server & saved & if it is successful then it is saved into the local list. NEXT - need to add page load which will load the current user's shoplists

--- wwwroot/js/shoplist.js
+++ wwwroot/js/shoplist.js
@@ -1,8 +1,44 @@
+let saveShopListUrl = "ShopList/Save";
+
 function addList(){
 
-    var title = document.querySelector("#listTitle").value;
+	// Add UUID to formdata
+	var formData = new FormData();
+	formData.append("uuid",currentUuid);
+
+	// Create new JSON shoplist
+	var shopList = {
+        Id:0,
+        Title: document.querySelector("#listTitle").value,
+        Created: new Date().yyyymmdd()
+    };
+
+    console.log(`${JSON.stringify(shopList)}`);
+    
+	// Add the new shoplist to the formdat
+    for (var key in shopList) {
+        formData.append(key, shopList[key]);
+    }
+
+
+	fetch(`${baseUrl}${saveShopListUrl}`,
+		{
+			method: 'POST',
+			body:formData,
+		
+			}).then(response => response.json())
+			.then(data => {
+				console.log(data);
+				if (data.success == true){
+					addItemToLocalShopList();
+				}
+			});
+}
+
+function addItemToLocalShopList(){
+	var title = document.querySelector("#listTitle").value;
     var localOption = new Option(title, title, false, true);
 		document.querySelector("#shopListCtrl").add(localOption);
 		document.querySelector("#listTitle").value = "";
-		document.querySelector("#SiteListCtrl").value = clearTextItemKey;  
+		document.querySelector("#SiteListCtrl").value = clearTextItemKey;
 }
