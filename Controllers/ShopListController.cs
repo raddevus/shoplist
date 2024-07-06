69ad90da141a7b2c8c1c3e7522f32f59402b6b15
raddevus <1radeutsch1@gmail.com>
2024-07-06 01:20:32 -0400

    In process, adding Save Api to ShopListController

--- Controllers/ShopListController.cs
+++ Controllers/ShopListController.cs
@@ -52,4 +52,48 @@ public class ShopListController : Controller
         return new JsonResult(allItems);
     }
 
+    [HttpPost]
+    public ActionResult Save([FromForm] String uuid,[FromForm] ShopList shopList){
+        
+        Console.WriteLine(shopList.Title);
+        
+        var userDir = Path.Combine(webRootPath,uuid);
+        var userDbFile = Path.Combine(userDir,templateDbFile);
+        // We need to get the OwnerId from sqlstone db using the UUID
+        var sqlstoneDb = Path.Combine(webRootPath,"sqlstone.db");
+        UuidInfoContext uic = new UuidInfoContext(sqlstoneDb);
+        UuidInfo? uinfo =  uic.Find<UuidInfo>(uuid);
+        if (uinfo == null){
+            return new JsonResult(new {success=false,error="The supplied UUID is not valid."});
+        }
+        
+        try{
+            ShopListContext slc = new ShopListContext(userDbFile);
+            // id = 0 indicates a new shopList
+            shopList.OwnerId = uinfo.Id;
+            if (shopList.Id == 0){
+                slc.Add(shopList);
+            }
+            // else{
+            //     ShopList? currentShopList = slc.Find<JournalEntry>(shopList.Id);
+            //     currentShopList.Note = shopList.Note;
+            //     currentShopList.Title = shopList.Title;
+            //     currentShopList.Updated = DateTime.Now.ToString("yyyy-MM-dd");
+            //     Console.WriteLine($"updated; {currentEntry.Updated}");
+            //     slc.Update(currentShopList);
+            //     shopList = currentShopList;
+            //     Console.WriteLine($"updated; {jentry.Updated}");
+
+            // }
+            slc.SaveChanges();
+            
+        }
+        catch (Exception ex){
+            // It's possible that the user has attempted to save an Entry
+            // but has never registered the UUID they see in their text box.
+            return new JsonResult(new {success=false,error=$"{ex.Message}"});
+        }
+
+        return new JsonResult(new {success=true,shopList=shopList});
+    }
 }
